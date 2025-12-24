0a9f3da0a569c658add50246b3ced94faf8bf3a9
PranayDhakate <85052290+PranayDhakate@users.noreply.github.com>
2025-12-24 16:13:02 +0530

    Tuples

---
+++ PythonLecture_06.py
@@ -0,0 +1,13 @@
+#Tuples in Python
+'''
+Tuples are similar to the list 
+but they are immutable, so cannot changed.
+For single value in tuple it should have comma ","
+'''
+
+tup = (1,2,34,345,4,62,642,434,3)
+print(type(tup))
+print(tup)
+print(tup[3])
+
+#tup[2] = 2 (Not Possible)
